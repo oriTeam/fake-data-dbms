03cce96d8dc46e714dbfcb59d8e010287b86a89a
VietNam102 <vantrong291@gmail.com>
2018-11-30 18:36:41 +0700

    update fake json

--- fake_firebase.py
+++ fake_firebase.py
@@ -1,10 +1,10 @@
-from faker import Faker
 import json
-
+from faker import Faker
 
 fake = Faker('en_US')
 
 import os
+
 PATH = str(os.getcwd()) + '/firebase_data/'
 
 while True:
@@ -19,7 +19,6 @@ staff = {}
 profile = {}
 credit = {}
 
-
 staff_file = PATH + "staff_file[" + str(start) + '_' + str(end) + "].json"
 profile_file = PATH + "profile_file[" + str(start) + '_' + str(end) + "].json"
 credit_file = PATH + "credit_file[" + str(start) + '_' + str(end) + "].json"
@@ -27,10 +26,17 @@ credit_file = PATH + "credit_file[" + str(start) + '_' + str(end) + "].json"
 for i in range(start, end):
     id = str(i)
 
+    staff_w = open(staff_file, "a")
+    profile_w = open(profile_file, "a")
+    credit_w = open(credit_file, "a")
+
     name = str(fake.name())
     staff[i] = {}
     staff[i]['id'] = id
     staff[i]['name'] = name
+    with open(staff_file, 'a') as fp:
+        json.dump(staff[i], fp)
+    staff_w.write(',')
 
     address = str(fake.city())
     dob = str(fake.date_this_century(before_today=True, after_today=False))
@@ -40,24 +46,16 @@ for i in range(start, end):
     profile[i]['address'] = address
     profile[i]['date_of_birth'] = dob
     profile[i]['phone'] = phone
+    with open(profile_file, 'a') as fp:
+        json.dump(profile[i], fp)
+    profile_w.write(',')
 
     card = str(fake.credit_card_number(card_type=None))
     credit[i] = {}
     credit[i]['id'] = id
     credit[i]['credit_card'] = card
+    with open(credit_file, 'a') as fp:
+        json.dump(credit[i], fp)
+    credit_w.write(',')
 
     print(i)
-
-staff_table = {"staff": staff}
-profile_table = {"profile": profile}
-credit_table = {"credit": credit}
-
-
-with open(staff_file, 'w') as fp:
-    json.dump(staff_table, fp)
-
-with open(profile_file, 'w') as fp:
-    json.dump(profile_table, fp)
-
-with open(credit_file, 'w') as fp:
-    json.dump(credit_table, fp)
